05f76ffb44b81e2f03c5e5787a6e14a12f96b063
Saad Arfan <82287213+MelonBun8@users.noreply.github.com>
2025-05-01 13:54:09 +0500

    Update server.js
    Mistake in server.js 
    was just require("./config/passport") Without assignment to any variable

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db")
 const userRoutes= require("./routes/userRoutes")
-require("./config/passport")
+const passport = require("./config/passport")
 const authRoutes = require("./routes/authRoutes");
 
 
